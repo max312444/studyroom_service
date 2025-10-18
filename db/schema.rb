f7553385d8e6e027f597c50da4bb911d1e1f139f
max312444 <max12max@g.yju.ac.kr>
2025-10-18 20:38:09 +0900

    10/18 commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_08_112209) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_12_094328) do
   create_table "reservations", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "room_id", null: false
     t.datetime "start_time"
@@ -39,6 +39,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_08_112209) do
     t.bigint "created_by", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "deleted_at"
     t.index ["holiday_date"], name: "index_room_exceptions_on_holiday_date"
     t.index ["room_id"], name: "index_room_exceptions_on_room_id"
   end
